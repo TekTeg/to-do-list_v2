acebaa237f2eaaa41a05eae9a01223fa14fb654c
Tekle <tekle@Mac.local>
2025-04-29 10:32:55 -0700

    home page css adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,52 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+  const [loginEmailSelected, setLoginEmailSelected] = useState(false);
+  const [loginPasswordSelected, setLoginPasswordSelected] = useState(false);
+  const login =async(e)=>{
+    e.preventDefault();
+    console.log('logging')
+  }
+  
   return (
-    <>
-    <h1>TO DO LIST</h1>
-    </>
+    
+    <div className='main-page'>
+      <h1>TO DO LIST</h1>
+      <h2>Login</h2>
+      <form onSubmit={login} className='login-form'>
+        <div className='login-email-container'>
+          {
+          loginEmailSelected && <p>email</p>
+          }
+          <input 
+            placeholder='email' type='email' 
+            onChange={(e) => setLoginEmail(e.target.value)} 
+            onSelect={(e)=>{e.target.placeholder='', setLoginEmailSelected(true)}}
+            onBlur={(e)=>{e.target.placeholder='email', setLoginEmailSelected(false)}}
+            />
+        </div>
+        <div className='login-password-container'>
+          {
+            loginPasswordSelected && <p>password</p>
+          }
+       
+          <input placeholder='password' type='password' 
+            onChange={(e) => setLoginPassword(e.target.value)} 
+            onSelect={(e)=>{e.target.placeholder='', setLoginPasswordSelected(true)}}
+            onBlur={(e)=>{e.target.placeholder='password', setLoginPasswordSelected(false)}}
+          
+          />
+        </div>
+        
+
+        <button>Login</button>
+      </form>
+     
+    </div>
   )
 }
 
